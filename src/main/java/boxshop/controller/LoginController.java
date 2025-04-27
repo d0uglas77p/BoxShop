cc76a6860e6a33abc6ae8402234d1623340f4ac7
Nemoske <luandevfreitas7@gmail.com>
2025-04-26 22:42:56 -0300

    Tratando recuperação de senha

---
+++ src/main/java/boxshop/controller/LoginController.java
@@ -0,0 +1,44 @@
+package boxshop.controller;
+
+import boxshop.services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LoginController {
+    @Autowired
+    private LoginService loginService;
+
+    @GetMapping("/recuperarSenha")
+    public String recuperarSenhaPag() {
+        return "recuperarSenha";
+    }
+
+    @PostMapping("/recuperar-conta-logista")
+    public String recuperarSenhaLog(@RequestParam("emailComercioLogista") String emailComercioLogista, RedirectAttributes redirectAttributes) {
+        try {
+            if (!loginService.isEmailCadastrado(emailComercioLogista)) {
+                redirectAttributes.addFlashAttribute("errorMessage", "Esse e-mail não está cadastrado!");
+                return "redirect:/inicio";
+            }
+
+            String token = loginService.gerarTokenRecuperacao(emailComercioLogista);
+
+            String urlRecuperacao = "http://localhost:8080/recuperarSenha?token=" + token;
+
+            loginService.enviarEmailRecuperacao(emailComercioLogista, "Clique no link para recuperar sua senha: " + urlRecuperacao);
+
+            redirectAttributes.addFlashAttribute("successMessage", "Um e-mail foi enviado com as instruções.");
+            return "redirect:/inicio";
+
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Erro ao Recuperar senha: "+e.getMessage());
+            return "redirect:/inicio";
+        }
+
+    }
+}
---
+++ src/main/java/boxshop/services/LoginService.java
@@ -0,0 +1,51 @@
+package boxshop.services;
+
+import boxshop.exception.EmailNaoCriadoException;
+import boxshop.repository.LoginRepository;
+import boxshop.repository.LogistaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class LoginService {
+    @Autowired
+    LogistaRepository logistaRepository;
+    @Autowired
+    LoginRepository loginRepository;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public boolean isEmailCadastrado(String emailComercioLogista){
+        return logistaRepository.existsByEmailComercioLogista(emailComercioLogista);
+    }
+
+    @Transactional
+    public String gerarTokenRecuperacao(String emailComercioLogista){
+        String token = UUID.randomUUID().toString();
+        LocalDateTime dataExpiracao = LocalDateTime.now().plusHours(1);
+        loginRepository.savarTokenRecuperacao(emailComercioLogista, token, dataExpiracao);
+
+        return token;
+    }
+
+    public void enviarEmailRecuperacao(String emailComercioLogista, String message){
+        if(!isEmailCadastrado(emailComercioLogista)){
+            throw new EmailNaoCriadoException("Email não cadastrado");
+        }
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(emailComercioLogista);
+        mailMessage.setSubject("Recuperação de senha");
+        mailMessage.setText(message);
+        javaMailSender.send(mailMessage);
+    }
+
+
+
+}
